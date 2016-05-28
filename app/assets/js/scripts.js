f555f6f6fb584b7101ec44480dd91092cd13e20c
Daan <daan@dvdzdesign.nl>
2016-05-28 13:32:52 +0200

    Clean start with Fastshell
    Installed:
    
    -Gulp
    -Sass
    -Fastshell

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * internsite
+ * Internsite
+ * https://internsite.dvdz.design
+ * @author Daan van der Zwaag
+ * @version 1.0.0
+ * Copyright 2016. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
